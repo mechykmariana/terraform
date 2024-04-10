e52b6845f2d336316b89e346cd5f366c388a95fe
mechykmariana <122735143+mechykmariana@users.noreply.github.com>
2024-04-10 14:12:21 +0300

    Add files via upload

---
+++ locals.tf
@@ -0,0 +1,33 @@
+# locals{
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     {
+#     sudo apt update
+#     sudo apt install -y apache2 php libapache2-mod-php php-mysql
+#     sudo apt install mysql-server
+#     sudo systemctl start apache2
+#     sudo systemctl enable apache2
+#     sudo mysql -u root -p
+#     rm /var/www/html/index.html
+#     sudo mv /var/www/html/index.html /var/www/html/index.html.old
+#     sudo mv /var/www/html/index.nginx-debian.html /var/www/html/index.nginx-debian.html.old
+
+#     sudo mv /tmp/index.php /var/www/html/index.php 
+#     sudo mv /tmp/index.html /var/www/html/index.html 
+
+#     CREATE DATABASE web;
+#     USE web;
+
+#     CREATE TABLE register (
+#         UserName varchar(255),
+#         Password varchar(255)
+#     );
+
+#     CREATE USER 'DBname'@'localhost' IDENTIFIED BY 'DBpassword';
+#     GRANT ALL PRIVILEGES ON web.* TO 'DBname'@'localhost';
+#     FLUSH PRIVILEGES;
+
+#     exit
+#     }
+#   EOF
+#   }
